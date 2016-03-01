37d1b6019dc5f3ce9a6652b2cda89cdcfa1edf93
troushoo <troushoo@gmail.com>
2016-03-01 13:51:26 +0000

    first commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        static void mymethod(long l){
+           long l1 = 1;
+           long l2 = 2;
+           long l3;
+
+           while(true){
+              l3 = l1 + l2;
+              l1 = l2 * l3;
+              l2 = l2 + l3;
+           }
+        }
+        public static void Main(string[] args)
+        {
+            mymethod(1);
+        }
+    }
+}
